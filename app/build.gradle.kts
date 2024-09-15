0bd569a05a67aa1da242dd75c459d838a0ed3071
Omar Gohary <omar.gohary123@gmail.com>
2024-09-15 22:16:56 +0200

    add nessessory dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,14 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs")
+    id("kotlin-kapt")
+
 }
 
 android {
     namespace = "com.example.weather_app_iti"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.weather_app_iti"
@@ -16,7 +19,10 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -36,8 +42,34 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    //coroutine
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.core:core-ktx:1.13.1")
+    // navigation component
+    implementation("androidx.navigation:navigation-fragment:2.8.0")
+    implementation("androidx.navigation:navigation-ui:2.8.0")
+    //room
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    // Gson
+    implementation("com.google.code.gson:gson:2.10.1")
+    // Retrofit Gson Converter
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    // circle imageview
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    // sdp
+    implementation("com.intuit.sdp:sdp-android:1.1.0")
+    //ssp
+    implementation("com.intuit.ssp:ssp-android:1.1.0")
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
+    //lottie
+    implementation("com.airbnb.android:lottie:6.2.0")
+    //----------------------------------------------------------
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
